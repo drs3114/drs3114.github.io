267e069de6243896c77b84d5b3c4f941dffb7615
DeepakShankar <drs3114@rit.edu>
2017-09-13 01:48:06 -0400

    my website for git pages

---
+++ js/particlebg.js
@@ -0,0 +1,13 @@
+/**
+ * This is the source JavaScript file which uses the particleground js to create
+ * the animated background.
+ */
+
+/*document.addEventListener('DOMContentLoaded', function() {
+	particleground(document.getElementById('particlesBody'), {
+		dotColor : '#5cbdaa',
+		lineColor : '#5cbdaa'
+	});
+	var intro = document.getElementById('intro');
+	intro.style.marginTop = -intro.offsetHeight / 2 + 'px';
+}, false);*/
---
+++ js/portfolio.js
@@ -0,0 +1,8 @@
+/**
+ * This source file is the property of Deepak Ravi Shankar and all the rights
+ * are reserved by me. Unintentional and illegal use of this file if prohibited
+ * and may be brought under the view of law
+ * 
+ * @author: Deepak Ravi Shankar
+ * @license: GPL Copyright 2016 Deepak Ravi Shankar
+ */
